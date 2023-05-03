fac8685466e7f69ba6cb2941fe62f0d10422a085
NiceCreeper3 <124564568+NiceCreeper3@users.noreply.github.com>
2023-05-03 14:27:35 +0200

    Ver2

--- Exam4.2/Exam4.2/Program.cs
+++ Exam4.2/Exam4.2/Program.cs
@@ -9,73 +9,106 @@ namespace Exam4._2
     {
         static void Main()
         {
+
+            #region
             GameSystem game = new GameSystem();
             game.UpdatePoints("player1", 100);
             game.UpdatePoints("player2", 150);
             game.UpdatePoints("player3", 75);
             game.UpdatePoints("player4", 200);
 
-            List<Player> sortedPlayers = game.GetPlayersSortedByPoints();
+            Player[] sortedPlayers = game.GetPlayersSortedByPoints();
             foreach (Player player in sortedPlayers)
             {
-                Console.WriteLine($"Player {player.PlayerName}: {player.Points} points");
+                Console.WriteLine($"Player {player.Id}: {player.Points} points");
             }
-
-
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Stop();
-
-
-            TimeSpan ts = stopWatch.Elapsed;
-
-            string elapsedTime = String.Format("{0:00}:{1:00}.{2:00}",
-            ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
-            Console.WriteLine("RunTime " + elapsedTime);
+            #endregion
         }
     }
 
-    // Bucket
-    class Player : IComparable<Player>
+    #region
+    //Element
+    class Player
     {
-        public string PlayerName { get; set; }
+        public string Id { get; set; }
         public int Points { get; set; }
 
-        public int CompareTo(Player other)
+        public Player(string id, int points)
         {
-            return other.Points.CompareTo(this.Points);
+            Id = id;
+            Points = points;
         }
     }
 
     class GameSystem
     {
-        private Dictionary<string, Player> playerDict;
-        private SortedSet<Player> playerSet;
+        private Player[] players;
+        private int count;
 
         public GameSystem()
         {
-            playerDict = new Dictionary<string, Player>();
-            playerSet = new SortedSet<Player>();
+            players = new Player[10]; // Definer størrelsen af arrayet efter behov
+            count = 0;
         }
 
         public void UpdatePoints(string playerId, int points)
         {
-            if (playerDict.ContainsKey(playerId))
+            // Find spilleren i arrayet eller indsæt den på den korrekte position
+            int index = FindPlayerIndex(playerId);
+            if (index != -1)
             {
-                Player player = playerDict[playerId];
-                player.Points = points;
+                players[index].Points = points;
             }
             else
             {
-                Player player = new Player { PlayerName = playerId, Points = points };
-                playerDict[playerId] = player;
-                playerSet.Add(player);
+                InsertPlayer(playerId, points);
             }
         }
 
-        public List<Player> GetPlayersSortedByPoints()
+        private int FindPlayerIndex(string playerId)
         {
-            return new List<Player>(playerSet);
+            for (int i = 0; i < count; i++)
+            {
+                if (players[i].Id == playerId)
+                {
+                    return i;
+                }
+            }
+            return -1;
         }
-    }
 
+        private void InsertPlayer(string playerId, int points)
+        {
+            // Find den rigtige position til indsættelse baseret på point
+            int insertIndex = 0;
+            while (insertIndex < count && points < players[insertIndex].Points)
+            {
+                insertIndex++;
+            }
+
+            // Flyt eksisterende spillere for at gøre plads til den nye spiller
+            for (int i = count - 1; i >= insertIndex; i--)
+            {
+                players[i + 1] = players[i];
+            }
+
+            // Indsæt den nye spiller
+            players[insertIndex] = new Player(playerId, points);
+            count++;
+        }
+
+        public Player[] GetPlayersSortedByPoints()
+        {
+            Player[] sortedPlayers = new Player[count];
+            Array.Copy(players, sortedPlayers, count);
+            Array.Sort(sortedPlayers, (p1, p2) => p2.Points.CompareTo(p1.Points));
+            return sortedPlayers;
+        }
+
+    }
+    #endregion
 }
+
+
+
+
